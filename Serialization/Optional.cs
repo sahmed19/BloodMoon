c65983f400731f216c8519653f05d7a6279fa70d
Sheehan Ahmed <sheehansahmed@gmail.com>
2025-04-07 02:24:54 -0700

    Update Optional.cs

--- Serialization/Optional.cs
+++ Serialization/Optional.cs
@@ -10,21 +10,22 @@ namespace BloodMoon.Serialization
         [SerializeField] public bool Enabled;
         [SerializeField] T Value;
 
+        public void Set(T value) => Value = value;
+
         public Optional(T initialValue)
         {
             Enabled = true;
             Value = initialValue;
         }
-        
+
         public static implicit operator bool(Optional<T> optional)
         {
             return optional.Enabled;
         }
-        
+
         public static implicit operator T(Optional<T> optional)
         {
             return optional.Value;
         }
-        
     }
 }
